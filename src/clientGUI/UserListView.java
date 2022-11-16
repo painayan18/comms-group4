6d09f911410845b71dede5f3ade634b1814f5700
Jared Patterson <jpatterson23@horizon.csueastbay.edu>
2022-11-16 11:39:42 -0800

    v3.1.1 members and methods
    Also commented in the structure/appearance of this view.

--- src/clientGUI/UserListView.java
+++ src/clientGUI/UserListView.java
@@ -1,5 +1,55 @@
 package clientGUI;
 
-public class UserListView {
+import javax.swing.*;
+import java.awt.*;
+import java.io.ObjectOutputStream;
+import java.util.List;
 
+import sharedData.UserData;
+
+public class UserListView implements Runnable {
+	GUI.State state;
+	
+	private ObjectOutputStream objectOutputStream;
+	private List<UserData> userData;
+
+	private JFrame frame;
+	private JSplitPane splitPaneUD; // up-down split
+	private JList<String> userList; // the up
+	private JPanel buttonPane; // the down
+	
+	private JButton backButton; 
+	private JButton addToChatRoomButton;
+	private JButton formNewChatRoomButton;
+	
+	/* splitPaneUD in frame
+	 *  __________________
+	 * |                  |
+	 * |userList          |
+	 * |                  |
+	 * |__________________|
+	 * |                  |
+	 * |buttonPane        |
+	 * ||back||add||form| |
+	 * |__________________|
+	 */
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+	private void requestUserData() {
+		
+	}
+	
+	private void formNewChatRoom() {
+		
+	}
+	
+	private void addUsersToChatRoom() {
+		
+	}
+	
 }
