c24c4573d2ddf6e9e63359a73b05cb8d055ce6ff
AjiBai <ajibahi@hotmail.com>
2022-11-28 13:18:39 +0500

    Revert "implemented"
    This reverts commit e8f8be46d4e2eefcf32f577a24918ca02b94ac64.

--- src/requestResponse/LoginMessage.java
+++ src/requestResponse/LoginMessage.java
@@ -1,41 +1,5 @@
 package requestResponse;
 
-import java.io.Serializable;
+public class LoginMessage {
 
-import sharedData.UserData;
-
-public class LoginMessage implements Serializable {
-	private String username;
-	private String password;
-	private String userDataAsString;
-	private boolean isIT;
-	
-	public LoginMessage(String username, String password, boolean isIT) {
-		this.username = username;
-		this.password = password;
-		this.userDataAsString = null;
-		this.isIT = isIT;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public UserData getUserData() throws Exception {
-		if (userDataAsString == null) return null;
-		return new UserData(userDataAsString);
-	}
-
-	public void setUserData(UserData userData) {
-		this.userDataAsString = userData.toString();
-	}
-
-	public boolean isIT() {
-		return isIT;
-	}
-	
 }
